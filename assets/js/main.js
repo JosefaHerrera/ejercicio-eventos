d8ab8e13f2d520090b8d7e4266060547e60f765b
JosefaHerrera <josefa@linuxero.cl>
2017-05-20 13:12:30 -0400

    comentando

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,16 @@ boton.addEventListener('click', function() {
     this.innerHTML = "<strong>" + "¡HOLA MUNDO!" + "<strong>";
 });
 
+//onload 
 function PaginaCargada(){
 	alert("la pagina esta cargada ! ")
 
 };
-
+//onmouseover
 function sobreMi(){
 	alert("Estas sobre mi ");
 };
-
+//onmouseout
 function noSobreMi(){
 	alert("No estas sobre mi ");
 };
