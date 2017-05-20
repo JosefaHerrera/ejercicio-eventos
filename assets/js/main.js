6826374fb552559771a6b7992b7d80832e940ea5
JosefaHerrera <josefa@linuxero.cl>
2017-05-20 13:02:55 -0400

    creando funciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-main.js
+
+//evento addEventListener Click
+var boton = document.getElementById('demo');
+boton.addEventListener('click', function() {
+    this.innerHTML = "<strong>" + "¡HOLA MUNDO!" + "<strong>";
+});
+
+function PaginaCargada(){
+	alert("la pagina esta cargada ! ")
+
+};
+
